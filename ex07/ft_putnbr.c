8ff157efe4e3107f597448359caccfe697f810b1
Joao pedro Ceia <jceia@c1r3s1.42lisboa.com>
2020-11-26 23:11:00 +0000

    ex07

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -1,34 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/26 22:33:22 by jceia             #+#    #+#             */
+/*   Updated: 2020/11/26 22:34:54 by jceia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
+	int i;
 
-    if (nb < 0)
-    {
-        char c = '-';
-        write(1, &c, 1);
-        nb = -nb;
-    }
-
-    int tmp = nb;
-    int nb_figures = 0;
-    while(tmp > 0)
-    {
-        tmp /= 10;
-        nb_figures++;
-    }
-
-    for(int i=0; i < nb_figures; i++)
-    {
-        int tmp = nb;
-        for(int j = i; j < nb_figures -1; j++)
-            tmp /= 10;
-        char c =  '0' + (tmp % 10);
-        write(1, &c, 1);
-    }
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	i = get_nr_figures(nb) - 1;
+	while (i >= 0)
+	{
+		print_digit_n(nb, i);
+		i--;
+	}
 }
 
-int main()
+int	get_nr_figures(int x)
 {
-    ft_putnbr(42);
+	int n_figures;
+
+	x = x < 0 ? -x : x;
+	n_figures = 0;
+	while (x > 0)
+	{
+		x /= 10;
+		n_figures++;
+	}
+	return (n_figures);
+}
+
+void	print_digit_n(int x, int n)
+{
+	char c;
+
+	while (n > 0)
+	{
+		x /= 10;
+		n--;
+	}
+	x = x % 10;
+	c = '0' + x;
+	write(1, &c, 1);
 }
