dc25e25b8807a901a781a7d190670ccc4d105d3a
Joao pedro Ceia <jceia@c1r1s6.42lisboa.com>
2020-11-28 19:42:29 +0000

    fix for special case of minimum int possible

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 22:33:22 by jceia             #+#    #+#             */
-/*   Updated: 2020/11/28 14:39:02 by jceia            ###   ########.fr       */
+/*   Updated: 2020/11/28 19:41:53 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,15 +44,22 @@ void	ft_putnbr(int nb)
 {
 	int i;
 
-	if (nb < 0)
+	if(nb == -2147483648)
 	{
-		write(1, "-", 1);
-		nb = -nb;
+		write(1, "-2147483648", 12);
 	}
-	i = get_nr_figures(nb) - 1;
-	while (i >= 0)
+	else
 	{
-		print_digit_n(nb, i);
-		i--;
+		if (nb < 0)
+		{
+			write(1, "-", 1);
+			nb = -nb;
+		}
+		i = get_nr_figures(nb) - 1;
+		while (i >= 0)
+		{
+			print_digit_n(nb, i);
+			i--;
+		}
 	}
 }
